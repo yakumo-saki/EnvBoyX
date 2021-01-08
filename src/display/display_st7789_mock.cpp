04cd8f8067f6faed0e495794419689fe3a38b021
Yakumo Saki <yakumo@ziomatrix.org>
2021-01-08 13:07:36 +0900

    fix warning

--- src/display/display_st7789_mock.cpp
+++ src/display/display_st7789_mock.cpp
@@ -11,13 +11,13 @@ void disp_st7789_normal_startup_screen(String product_long) {}
 void disp_st7789_wait_for_reconfig_init() {}
 void disp_st7789_wait_for_reconfig_bar(int now, const int max) {}
 void disp_st7789_wifi_starting(int wait_print_row) {}
-void disp_st7789_all_initialize_complete(String ip, String mdns) {}
-void disp_st7789_sensor_value(disp_values_t new_values, disp_values_t last_values) {}
-void disp_st7789_power_off() {}
+void disp_st7789_all_initialize_complete(String ip, String mdns) {};
+void disp_st7789_sensor_value(disp_values_t new_values, disp_values_t last_values) {};
+void disp_st7789_power_off() {};
 
-void disp_st7789_wifi_error() {}
+void disp_st7789_wifi_error() {};
 
-String disp_st7789_set_brightness(int brightness) {}
+String disp_st7789_set_brightness(int brightness) { return ""; }
 
 /**
  * 液晶のON/OFFを設定する
