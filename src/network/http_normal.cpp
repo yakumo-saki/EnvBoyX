9b397486d201c782f753e695889629583d61263a
Yakumo Saki <yakumo@ziomatrix.org>
2021-01-09 00:56:03 +0900

    FIX: http json wrong time value

--- src/network/http_normal.cpp
+++ src/network/http_normal.cpp
@@ -8,23 +8,35 @@ String http_normal_not_found_html() {
   return message;
 }
 
-String http_normal_data_json() {
+String get_time_string(unsigned long ms) {
+  unsigned long second = ms / 1000;
+  unsigned long minute = second / 60;
+
+  int sec = second % 60;
+  int min = (second / 60) % 60;
+  int hr =  minute / 60;
 
   char buf[400];
-  int sec = millis() / 1000;
-  int min = (int) ((sec / 60) % 60);
-  int hr = (int) ((min / 60) % 60);
+  snprintf (buf, sizeof buf,"%02d:%02d:%02d", hr, min, sec);
+  return String(buf);
+}
+
+String http_normal_data_json() {
+
+  unsigned long ms = millis();
+  String timeString = get_time_string(ms);
 
   char temp[16], hum[16], pres[16];
   char lux[16], luxIr[16],ppm[16];
   char product[32];
   product_long.toCharArray(product, sizeof(product), 0);
 
+  char buf[400];
   snprintf ( buf, sizeof buf,
-    "{ \"product\": \"%s\", \"uptime\": \"%02d:%02d:%02d\", \"uptimeMills\": \"%lu\", \"temparature\": \"%s\","
+    "{ \"product\": \"%s\", \"uptime\": \"%s\", \"uptimeMills\": \"%lu\", \"temparature\": \"%s\","
     " \"humidity\": \"%s\", \"pressure\": \"%s\",\"luminous\": \"%s\", \"luminousIr\": \"%s\""
     ", \"co2ppm\": \"%s\", \"co2ppmAccuracy\": \"%s\" }"
-    , product, hr, min, sec, millis()
+    , product, timeString.c_str(), ms
     , dtostrf(sensorValues.temperature, 0, 2, temp)
     , dtostrf(sensorValues.humidity, 0, 2, hum)
     , dtostrf(sensorValues.pressure, 0, 2, pres)
@@ -42,13 +54,12 @@ String http_normal_data_json() {
 String http_normal_ping_json() {
 
   char temp[100];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
+  unsigned long ms = millis();
+  String timeString = get_time_string(ms);
 
   snprintf ( temp, sizeof temp,
-    "{ \"uptime\": \"%02d:%02d:%02d\", \"uptimeMills\": \"%lu\" }\n"
-    , hr, min % 60, sec % 60, millis()
+    "{ \"uptime\": \"%s\", \"uptimeMills\": \"%lu\" }\n"
+    , timeString.c_str(), ms
   );
 
   String json = "return ping: " + String(temp);
