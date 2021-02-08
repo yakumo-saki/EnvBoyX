3773c17673f88f5ee4bc949c4c9eb6353a7652dc
Yakumo Saki <yakumo@ziomatrix.org>
2021-02-09 01:17:05 +0900

    fIX: compile error

--- src/display/display_st7789_mode_mock.cpp
+++ src/display/display_st7789_mode_mock.cpp
@@ -6,7 +6,7 @@
 #include "global.h"
 #include "structs.h"
 
-void disp_st7789_setup_startup_screen(String ipAddr) {}
+void disp_st7789_setup_startup_screen(String ipAddr, int disp_switch) {}
 
 void disp_st7789_wifi_info(String ip, String mDNS) {}
 void disp_st7789_normal_startup_screen(String product_long) {}
