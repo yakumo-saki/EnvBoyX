6da654da9dbcc58829d4907f1798a1651e7137d9
Yakumo Saki <yakumo@ziomatrix.org>
2021-03-23 20:08:58 +0900

    ADD: autodimmer implementation

--- src/display/autodimmer.cpp
+++ src/display/autodimmer.cpp
@@ -7,9 +7,51 @@
 
 #include "global.h"
 #include "log.h"
+#include "config.h"
+#include "display/display.h"
 
+unsigned int belowThreasholdSecond = 0;
+
+int lastBrightness = 255;
+
+// 減光中か否か
+bool dimming = false;
+
+// * AutoDimLux 以下のLux状態がWaitSecond秒続いたら減光
+// * 減光中にAutoDimLuxを超えたLuxになったら即減光解除
+// * 原稿中にAutoDimLux以下のLuxが継続していたら減光続行
 void autodimmer_loop() {
 
-    displog("Autodimmer: lux=" + String(sensorValues.lux));
+    int minLux = config.displayAutoDimLux.toInt();
+    int waitSecond = config.displayAutoDimWaitSecond.toInt();
 
+    // displog("Autodimmer: lux=" + String(sensorValues.lux) + " minLux=" + String(minLux));
+
+    // 明るいか否か
+    if (sensorValues.lux > minLux) {
+        belowThreasholdSecond = 0;
+        if (dimming) {
+            // 減光解除
+            dimming = false;
+            disp_set_brightness(lastBrightness);
+        }
+
+        return;
+    }
+
+    // 暗い
+    if (dimming) {
+        return; // 減光中なのでそのまま減光
+    }
+
+    // 暗い状態が継続しているか
+    belowThreasholdSecond++;
+    // debuglog(String(belowThreasholdSecond));
+    if (belowThreasholdSecond >= waitSecond) {
+        // 減光する
+        dimming = true;
+
+        lastBrightness = config.displayBrightness.toInt();  // 本当はこの時点の明るさがほしいが取得できないので仮おき
+        disp_set_brightness(0);
+    }
 }
