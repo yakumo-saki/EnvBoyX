5d885ca03eb099dca4fa0f6fabf93af00ab0a0e3
Yakumo Saki <yakumo@ziomatrix.org>
2022-12-11 00:31:37 +0900

    fix wrong diff value on boot

--- src/sensors/delta.cpp
+++ src/sensors/delta.cpp
@@ -11,7 +11,7 @@
 const int MAX_HISTORY = 30;
 
 // ここに毎分のデータを保存する。
-std::vector<sensor_values_t> history(MAX_HISTORY);
+std::vector<sensor_values_t> history;
 
 void store_history() {
     if (history.size() >= MAX_HISTORY) {
@@ -22,54 +22,36 @@ void store_history() {
     // deltalog("Pressure history pushed. historySize = " + String(history.size()));
 }
 
-void store_zero_delta() {
-    deltaValues.pressure = 0.0;
-    deltaValues.temperature = 0.0;
-    deltaValues.humidity = 0.0;
-    deltaValues.pressure = 0.0;
-    deltaValues.lux = 0.0;
-    deltaValues.co2ppm = 0;
-}
-
 void store_delta() {
-    if (history.size() == 0) {
-        store_zero_delta();
-        deltalog(F("No history. Assume delta = 0"));
-        return; 
+    // 起動直後のhistoryが空の状態のときに、最新の測定値を
+    // historyに追加して、差分の計算を単純にする
+    if (history.empty()) {
+        deltalog(F("No history. Create first history"));
+        history.reserve(MAX_HISTORY);
+        store_history();
+    } else {
+        // statlog("history size=" + String(history.size())
+        //   + " Capacity="  + String(history.capacity()));
     }
 
-    const sensor_values_t* base = nullptr;
+    const sensor_values_t *base = nullptr;
+    base = &history.front();
 
-    for (const sensor_values_t v : history) {
-        // 有効なデータが入ってるかチェックして、入っていればdeltaを計算する元にする
-        if (v.temperature != 0.0 || v.pressure != 0.0) {
-            base = &v;
-            break;
-        }
-    }
-
-    if (base != nullptr) {
-        // 計算
-        deltaValues.pressure = sensorValues.pressure - base->pressure;
-        deltaValues.temperature = sensorValues.temperature - base->temperature;
-        deltaValues.humidity = sensorValues.humidity - base->humidity;
-        deltaValues.lux = sensorValues.lux - base->lux;
-        deltaValues.co2ppm = sensorValues.co2ppm - base->co2ppm;
+    // 計算
+    deltaValues.pressure = sensorValues.pressure - base->pressure;
+    deltaValues.temperature = sensorValues.temperature - base->temperature;
+    deltaValues.humidity = sensorValues.humidity - base->humidity;
+    deltaValues.lux = sensorValues.lux - base->lux;
+    deltaValues.co2ppm = sensorValues.co2ppm - base->co2ppm;
 
 #ifdef SENSOR_VALUE_LOG
-        deltalog("Pressure Delta=" + String(sensorValues.pressure) 
-                + " PRES=" + String(sensorValues.pressure, 2) 
-                + " BASE=" + String(base->pressure, 2)
-                + " FORMATTED=" + format_delta_value(deltaValues.pressure));
-        deltalog("temperature Delta=" + String(sensorValues.temperature) 
-                + " PRES=" + String(sensorValues.temperature, 2) 
-                + " BASE=" + String(base->temperature, 2)
-                + " FORMATTED=" + format_delta_value(deltaValues.temperature));
+    deltalog("Pressure Delta=" + String(sensorValues.pressure) 
+            + " PRES=" + String(sensorValues.pressure, 2) 
+            + " BASE=" + String(base->pressure, 2)
+            + " FORMATTED=" + format_delta_value(deltaValues.pressure));
+    deltalog("temperature Delta=" + String(sensorValues.temperature) 
+            + " PRES=" + String(sensorValues.temperature, 2) 
+            + " BASE=" + String(base->temperature, 2)
+            + " FORMATTED=" + format_delta_value(deltaValues.temperature));
 #endif
-    } else {
-        // 計算
-        store_zero_delta();
-    }
-  
-
 }
