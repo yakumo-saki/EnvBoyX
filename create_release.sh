79b7008b78ecb43ad4bcb15b230c15e03471b274
Yakumo Saki <yakumo@ziomatrix.org>
2022-12-30 21:15:49 +0900

    update release script

--- create_release.sh
+++ create_release.sh
@@ -6,6 +6,8 @@ RELEASE_DIR=${RELEASE_BASE_DIR}/EnvBoyX
 SRC_BUILD_DIR=${MYDIR}/.pio/build
 TGT_BUILD_DIR=${RELEASE_DIR}/.pio/build
 
+# SUBs
+
 banner () {
   echo ""
   echo "================================================="
@@ -16,39 +18,37 @@ banner () {
 copy_src_to_tgt () {
   banner "Copying board build $1"
 
-  # ESP32 dev board
+  # $1 = boardname
   mkdir -p ${TGT_BUILD_DIR}/$1
-  cp -v ${SRC_BUILD_DIR}/$1/*.bin ${TGT_BUILD_DIR}/$1/
+  cp -v ${SRC_BUILD_DIR}/$1/firmware.bin ${TGT_BUILD_DIR}/$1_EnvBoyX_$VER$VER_ADDITIONAL.bin
 }
 
-banner "Delete source build dir and clean build"
-rm -rf ${MYDIR}/.pio
+# --------------------
+# MAIN
+# --------------------
 cd ${MYDIR}
-pio run
 
-banner "Delete and create build directory"
-rm -rf ${RELEASE_BASE_DIR}
+# -------------------------------------------------------------------------
+banner "Get Version"
+# -------------------------------------------------------------------------
+MAJOR=$(grep " ver =" ./src/global.cpp | sed -r 's/.*"(.*+)".*/\1/')
+MINOR=$(grep " minorVer =" ./src/global.cpp | sed -r 's/.*"(.*+)".*/\1/')
+VER=$(echo "${MAJOR}.${MINOR}")
+echo "Version is ${VER}"
 
-mkdir -p ${RELEASE_DIR}
 
-cp ./platformio.ini ${RELEASE_DIR}/
-
-BOARDS=( esp32dev esp12e )
-
-for b in ${BOARDS[@]} ; do
-  copy_src_to_tgt $b
-done
-
-banner "Add 'how to write' to build directory"
-cp ${MYDIR}/docs/how_to_write*.md ${RELEASE_DIR}
-
-banner "Creating Archive"
-
-rm -f ${MYDIR}/EnvBoyX*.tar.gz
-
+# -------------------------------------------------------------------------
+banner "Get Version additional info (debug, branch...)"
+# -------------------------------------------------------------------------
 # git ブランチからファイル名判定
 branch=`git symbolic-ref --short HEAD`
+if [ $branch != "main" ]; then
+  echo "git branch is not master/main!"
+  echo "Appending branch name"
+  VER_ADDITIONAL=$branch
+fi
 
+# DEBUGビルド判定
 set +e
 grep -i "DEBUG_BUILD.*TRUE" ${MYDIR}/src/global.cpp
 IS_DEBUG=$?
@@ -56,29 +56,34 @@ set -e
 
 if [ $IS_DEBUG -eq 0 ] ; then
   echo "** DEBUG_BUILD IS SET TO TRUE"
-  RELEASE_FILE_PATH=${MYDIR}/EnvBoyX-DEBUG.tar.gz
-elif [ $branch = "main" ]; then
-  RELEASE_FILE_PATH=${MYDIR}/EnvBoyX.tar.gz
-else
-  echo "git branch is not master/main!"
-  echo "Appending branch name"
-  RELEASE_FILE_PATH=${MYDIR}/EnvBoyX-$branch.tar.gz
+
+  if [ ${#VER_ADDITIONAL} -eq 0 ] ; then
+    VER_ADDITIONAL=DEBUG
+  else
+    VER_ADDITIONAL=$(echo "${VER_ADDITIONAL}-DEBUG")
+  fi
 fi
 
-rm -f ${MYDIR}/EnvBoyX*.tar.gz
-tar -C ${RELEASE_BASE_DIR} -zcvf ${RELEASE_FILE_PATH} .
-
-banner "Release archive created ${RELEASE_FILE_PATH}"
-
-# Warning
-
-echo "To upload, run:"
-echo "tar xvf `basename ${RELEASE_FILE_PATH}`"
-echo "cd `basename ${RELEASE_FILE_PATH}`"
-echo "pio run -t nobuild -t upload --disable-auto-clean"
-echo "If you want to specify board, use below."
-echo "pio run -e [esp32dev | esp12e] -t nobuild -t upload --disable-auto-clean"
-
-if [ $IS_DEBUG -eq 1 ] ; then
-  banner "RELEASE VERSION ! Nice work !"
+if [ ${#VER_ADDITIONAL} -gt 0 ] ; then
+  VER_ADDITIONAL=$(echo "-$VER_ADDITIONAL")
 fi
+
+# -------------------------------------------------------------------------
+banner "Delete and create build directory"
+# -------------------------------------------------------------------------
+rm -rf ${RELEASE_BASE_DIR}
+mkdir -p ${RELEASE_DIR}
+
+# -------------------------------------------------------------------------
+banner "Build"
+# -------------------------------------------------------------------------
+
+BOARDS=( esp32dev esp12e )
+
+#pio run --target clean
+pio run --environment esp32dev  --environment esp12e
+
+for b in ${BOARDS[@]} ; do
+  copy_src_to_tgt $b
+done
+
