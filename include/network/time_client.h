0f72a92d58832b9f2df299edd592e860b3724528
Yakumo Saki <yakumo@ziomatrix.org>
2023-03-06 01:43:29 +0900

    close #137

--- include/network/time_client.h
+++ include/network/time_client.h
@@ -7,4 +7,7 @@ String getDate();
 String getTime();
 
 /** time is not set due to NTP failed or NTP disabled */
-const String TIME_NOT_READY = "0000/00/00 00:00:00";
+const String DATETIME_NOT_READY = "0000/00/00 00:00:00";
+
+/** time is not set due to NTP failed or NTP disabled */
+const String TIME_NOT_READY = "00:00:00";
